ab2bc32c5c73028a5a11a21742691a37a2f5533b
vprahul2008 <73024199+vprahul2008@users.noreply.github.com>
2021-01-08 09:34:51 +0530

    Add files via upload
    white hat project

---
+++ Bob.js
@@ -0,0 +1,25 @@
+class Bob {
+
+    constructor(x,y){
+        var options = {
+            isStatic: false,
+            restitution: 1,
+            friction: 0,
+            density: 7.8,
+        }
+        this.body = Bodies.circle(x,y,25,options);
+        World.add(world, this.body);
+
+        console.log(this.body);
+    
+    }
+    
+    display(){
+        push();
+        ellipseMode(RADIUS);
+        fill("skyblue");
+        ellipse(this.body.position.x,this.body.position.y,25,25);
+        pop();
+    }
+    
+    }
